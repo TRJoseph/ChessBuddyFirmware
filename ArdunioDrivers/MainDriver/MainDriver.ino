c06eefb05b0c71b65c9c5330c334a1f9a152e12d
Thomas Joseph <thomas@aeriehub.com>
2024-06-07 15:40:21 -0400

    small progress

--- ArdunioDrivers/MainDriver/MainDriver.ino
+++ ArdunioDrivers/MainDriver/MainDriver.ino
@@ -1,45 +1,126 @@
 #include <Servo.h>
 #include <Arduino.h>
 
-Servo servo1;  // create servo object to control a servo
-
+Servo baseServo;
+Servo mainArmServo;
+Servo leverArmServo;
 
 int pos = 0;  // variable to store the servo position
 
-void testRun() {
-  delay(500);
+// void testRun() {
+//   delay(500);
 
-  for (pos = 110; pos >= 55; pos -= 1) {  // goes from 180 degrees to 0 degrees
+//   for (pos = 110; pos >= 55; pos -= 1) {  // goes from 180 degrees to 0 degrees
 
-    servo1.write(pos);  // tell servo to go to position in variable 'pos'
+//     servo1.write(pos);  // tell servo to go to position in variable 'pos'
 
-    delay(100);  // waits 15ms for the servo to reach the position
-  }
+//     delay(100);  // waits 15ms for the servo to reach the position
+//   }
 
-  digitalWrite(7, HIGH);
-  delay(1000);
+//   digitalWrite(7, HIGH);
+//   delay(1000);
 
-  for (pos = 55; pos <= 110; pos += 1) {  
+//   for (pos = 55; pos <= 110; pos += 1) {  
 
-    servo1.write(pos); 
+//     servo1.write(pos); 
 
-    delay(100);  // waits 15ms for the servo to reach the position
-  }
+//     delay(100);  // waits 15ms for the servo to reach the position
+//   }
 
-  digitalWrite(7, LOW);
-  delay(1000);
+//   digitalWrite(7, LOW);
+//   delay(1000);
+
+// }
+
+// void testBase() {
+//     delay(1000);
+//     double desiredAngle = 63.43;  // Target angle in degrees
+//     double gearRatio = 2.27;  // Gear ratio
+//     double motorAngle = desiredAngle / gearRatio;
+
+//     int currentPos = baseServo.read();  // Read current position of servo
+//     int targetPos = int(180 - motorAngle);  // Calculate target position considering gear inversion
+
+//     if (currentPos < targetPos) {
+//         for (pos = currentPos; pos <= targetPos; pos++) {  // Increment position
+//             baseServo.write(pos);
+//             delay(15);  // Short delay to allow the servo to reach the position
+//         }
+//     } else {
+//         for (pos = currentPos; pos >= targetPos; pos--) {  // Decrement position
+//             baseServo.write(pos);
+//             delay(15);  // Short delay to allow the servo to reach the position
+//         }
+//     }
+// }
+
+void testMainArm() {
+    int targetPos = 86;
+
+    targetPos = int(180 - targetPos);
+    int currentPos = mainArmServo.read();  // Read current position of servo
+
+    if (currentPos < targetPos) {
+        for (pos = currentPos; pos <= targetPos; pos++) {  // Increment position
+            mainArmServo.write(pos);
+            delay(15);  // Short delay to allow the servo to reach the position
+        }
+    } else {
+        for (pos = currentPos; pos >= targetPos; pos--) {  // Decrement position
+            mainArmServo.write(pos);
+            delay(15);  // Short delay to allow the servo to reach the position
+        }
+    }
+}
+
+void testLeverArm() {
+    int targetPos = 100;
+
+    int currentPos = leverArmServo.read();  // Read current position of servo
+
+    if (currentPos < targetPos) {
+        for (pos = currentPos; pos <= targetPos; pos++) {  // Increment position
+            leverArmServo.write(pos);
+            delay(15);  // Short delay to allow the servo to reach the position
+        }
+    } else {
+        for (pos = currentPos; pos >= targetPos; pos--) {  // Decrement position
+            leverArmServo.write(pos);
+            delay(15);  // Short delay to allow the servo to reach the position
+        }
+    }
+}
+
+// in mm
+int mainVertArmLength = 135;
+int mainHoriArmLength = 145;
+
+// length from the servo's pivot to the connection point on the lever arm
+int leverLength = 56;
+
+// length from the lever arm's endpoint through the linkage to Joint 2
+int linkLength = 135;
+
+void positionDriver(int x, int y, int z) {
 
 }
+
 
 void setup() {
 
   pinMode(7, OUTPUT);
 
-  servo1.attach(8);  // attaches the servo on pin 9 to the servo object
-
+  baseServo.attach(8);  // attaches the servo on pin 9 to the servo object
+  mainArmServo.attach(9);  // attaches the servo on pin 9 to the servo object
+  leverArmServo.attach(10);  // attaches the servo on pin 9 to the servo object
   //digitalWrite(7, HIGH);
-  testRun();
+  //testRun();
+  //testBase();
+  testMainArm();
+  testLeverArm();
 }
+
+
 
 
 void loop() {
@@ -52,7 +133,7 @@ void loop() {
 
 //     // in steps of 1 degree
 
-//     servo1.write(pos);              // tell servo to go to position in variable 'pos'
+//     baseServo.write(pos);              // tell servo to go to position in variable 'pos'
 
 //     delay(15);                       // waits 15ms for the servo to reach the position
 
@@ -60,7 +141,7 @@ void loop() {
 
 //   for (pos = 110; pos >= 55; pos -= 1) { // goes from 180 degrees to 0 degrees
 
-//     servo1.write(pos);              // tell servo to go to position in variable 'pos'
+//     baseServo.write(pos);              // tell servo to go to position in variable 'pos'
 
 //     delay(15);                       // waits 15ms for the servo to reach the position
 
