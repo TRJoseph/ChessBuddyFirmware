73f30366b2bad4d6c007b2c6fc8c06a4ef9a3d6e
Thomas Joseph <trjoseph2002@gmail.com>
2025-07-05 11:00:33 -0400

    Fix for render environment port

--- python/src/CommunicationInterface.py
+++ python/src/CommunicationInterface.py
@@ -82,4 +82,6 @@ def get_move():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    # attempts to get render port set, falls back to 5000
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
